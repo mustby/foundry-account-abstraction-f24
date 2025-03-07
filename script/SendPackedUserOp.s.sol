f79ba9f255a265a6aeb57ba8149f84a9bce0ca73
mustby <mjustby@gmail.com>
2025-03-06 20:31:56 -0600

    Initial commit

---
+++ script/SendPackedUserOp.s.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Script} from "lib/forge-std/src/Script.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
+// import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+// import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
+// import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol"; // need to install foundry devops tool?
+
+contract SendPackedUserOp is Script {
+    using MessageHashUtils for bytes32;
+
+    // address private constant RANDOM_APPROVER =
+    //     0x9EA9b0cc1919def1A3CfAEF4F7A66eE3c36F86fC;
+
+    // function run() public {
+    //     // Setup
+    //     HelperConfig helperConfig = new HelperConfig();
+    //     address dest = helperConfig.getConfig().usdc; // arbitrum mainnet USDC address
+    //     uint256 value = 0;
+    //     address minimalAccountAddress = DevOpsTools.get_most_recent_deployment(
+    //         "MinimalAccount",
+    //         block.chainid
+    //     );
+
+    //     bytes memory functionData = abi.encodeWithSelector(
+    //         IERC20.approve.selector,
+    //         RANDOM_APPROVER,
+    //         1e18
+    //     );
+    //     bytes memory executeCalldata = abi.encodeWithSelector(
+    //         MinimalAccount.execute.selector,
+    //         dest,
+    //         value,
+    //         functionData
+    //     );
+    //     PackedUserOperation memory userOp = generateSignedUserOperation(
+    //         executeCalldata,
+    //         helperConfig.getConfig(),
+    //         minimalAccountAddress
+    //     );
+    //     PackedUserOperation[] memory ops = new PackedUserOperation[](1);
+    //     ops[0] = userOp;
+
+    //     // Send transaction
+    //     vm.startBroadcast();
+    //     IEntryPoint(helperConfig.getConfig().entryPoint).handleOps(
+    //         ops,
+    //         payable(helperConfig.getConfig().account)
+    //     );
+    //     vm.stopBroadcast();
+    // }
+
+    function generateSignedUserOperation(
+        bytes memory callData,
+        HelperConfig.NetworkConfig memory config,
+        address minimalAccount
+    ) public view returns (PackedUserOperation memory) {
+        // 1. Generate the unsigned data
+        uint256 nonce = vm.getNonce(minimalAccount) - 1;
+        PackedUserOperation memory userOp = _generateUnsignedUserOperation(
+            callData,
+            minimalAccount,
+            nonce
+        );
+
+        // 2. Get the userOp Hash
+        bytes32 userOpHash = IEntryPoint(config.entryPoint).getUserOpHash(
+            userOp
+        );
+        bytes32 digest = userOpHash.toEthSignedMessageHash();
+
+        // 3. Sign it, and return it
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint256 anvilDefaultKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        if (block.chainid == 31337) {
+            (v, r, s) = vm.sign(anvilDefaultKey, digest);
+        } else {
+            (v, r, s) = vm.sign(config.account, digest);
+        } // need to be clever here with local chains...
+        userOp.signature = abi.encodePacked(r, s, v); // Note the order here
+        return userOp;
+    }
+
+    function _generateUnsignedUserOperation(
+        bytes memory callData,
+        address sender,
+        uint256 nonce
+    ) internal pure returns (PackedUserOperation memory) {
+        uint128 verificationGasLimit = 16777216;
+        uint128 callGasLimit = verificationGasLimit;
+        uint128 maxPriorityFeePerGas = 256;
+        uint128 maxFeePerGas = maxPriorityFeePerGas;
+
+        return
+            PackedUserOperation({
+                sender: sender,
+                nonce: nonce,
+                initCode: hex"",
+                callData: callData,
+                accountGasLimits: bytes32(
+                    (uint256(verificationGasLimit) << 128) | callGasLimit
+                ),
+                preVerificationGas: verificationGasLimit,
+                gasFees: bytes32(
+                    (uint256(maxPriorityFeePerGas) << 128) | maxFeePerGas
+                ),
+                paymasterAndData: hex"",
+                signature: hex""
+            });
+    }
+}
---
+++ src/zksync/ZkMinimalAccount.sol
@@ -0,0 +1,195 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+// zkSync ERA Imports
+import {IAccount, ACCOUNT_VALIDATION_SUCCESS_MAGIC} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+import {Transaction, MemoryTransactionHelper} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import {SystemContractsCaller} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
+import {NONCE_HOLDER_SYSTEM_CONTRACT, BOOTLOADER_FORMAL_ADDRESS, DEPLOYER_SYSTEM_CONTRACT} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import {INonceHolder} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
+import {Utils} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/Utils.sol";
+
+// OZ Imports
+import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+/**
+ * Lifecycle of a type 113 (0x71) txn
+ * msg.sender is always the bootloader system contract
+ *
+ * Phase 1 Validation
+ * 1. User sends the txn to the zkSync API client (sort of a "light node")
+ * 2. The zkSync API client checks to see that the nonce is unique by querying the Nonceholder system contract (NonceHolder.sol)
+ * 3. The zkSync API client calls validateTransaction, which MUST update the nonce
+ * 4. The zkSync API client checks the nonce is updated
+ * 5. The zkSync API client calls payForTransaction, or prepareForPaymaster & validateAndPayForPaymasterTransaction
+ * 6. The zkSync API client verifies that the bootloader gets paid
+ *
+ * Phase 2 Execution
+ * 7. The zkSync API client passes the validated transaction to the main node / sequencer (as of today, they are the same)
+ * (ZkSync is working on decentralizing their sequencer --> What does this mean?)
+ * 8. The main node will call the executeTransaction function
+ * 9. If a paymaster was used, the postTransaction function will be called
+ */
+
+contract ZkMinimalAccount is IAccount, Ownable {
+    using MemoryTransactionHelper for Transaction;
+
+    error ZkMinimalAccount__NotEnoughBalance();
+    error ZkMinimalAccount__NotFromBootLoader();
+    error ZkMinimalAccount__ExecutionFailed();
+    error ZkMinimalAccount__NotFromBootLoaderOrOwner();
+    error ZkMinimalAccount__FailedToPay();
+    error ZkMinimalAccount__InvalidSignature();
+
+    /*/////////////////////////////////////////////////////////////////
+    /////////////////      Modifiers     //////////////////////////////
+    /////////////////////////////////////////////////////////////////*/
+
+    modifier requireFromBootLoader() {
+        if (msg.sender != BOOTLOADER_FORMAL_ADDRESS) {
+            revert ZkMinimalAccount__NotFromBootLoader();
+        }
+        _;
+    }
+
+    modifier requireFromBootLoaderOrOwner() {
+        if (msg.sender != BOOTLOADER_FORMAL_ADDRESS && msg.sender != owner()) {
+            revert ZkMinimalAccount__NotFromBootLoaderOrOwner();
+        }
+        _;
+    }
+
+    constructor() Ownable(msg.sender) {}
+
+    receive() external payable {}
+
+    /*/////////////////////////////////////////////////////////////////
+    /////////////////     External Functions     //////////////////////
+    /////////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice We must increase the nonce
+     * @notice We must validate the transaction (check the owner signed the transaction)
+     * @notice also check to see if we have enough money in our account
+     */
+
+    function validateTransaction(
+        bytes32 /*_txHash*/,
+        bytes32 /*_suggestedSignedHash*/,
+        Transaction memory _transaction // we will learn this first!
+    ) external payable requireFromBootLoader returns (bytes4 magic) {
+        return _validateTransaction(_transaction);
+    }
+
+    function executeTransaction(
+        bytes32 /*_txHash*/,
+        bytes32 /*_suggestedSignedHash*/,
+        Transaction memory _transaction
+    ) external payable requireFromBootLoaderOrOwner {
+        _executeTransaction(_transaction);
+    }
+
+    function executeTransactionFromOutside(
+        Transaction memory _transaction
+    ) external payable {
+        bytes4 magic = _validateTransaction(_transaction);
+        if (magic != ACCOUNT_VALIDATION_SUCCESS_MAGIC) {
+            revert ZkMinimalAccount__InvalidSignature();
+        }
+        _executeTransaction(_transaction);
+    }
+
+    function payForTransaction(
+        bytes32 /*_txHash*/,
+        bytes32 /*_suggestedSignedHash*/,
+        Transaction memory _transaction
+    ) external payable {
+        bool success = _transaction.payToTheBootloader(); // PC used a lower case L here...be wary...
+        if (!success) {
+            revert ZkMinimalAccount__FailedToPay();
+        }
+    }
+
+    function prepareForPaymaster(
+        bytes32 _txHash,
+        bytes32 _possibleSignedHash,
+        Transaction memory _transaction
+    ) external payable {}
+
+    /*/////////////////////////////////////////////////////////////////
+    /////////////////     Internal Functions     //////////////////////
+    /////////////////////////////////////////////////////////////////*/
+
+    function _validateTransaction(
+        Transaction memory _transaction
+    ) internal returns (bytes4 magic) {
+        // Call nonceholder
+        // Increment nonce
+        // call (x, y, z) --> system contract call //// zkSync simulations
+        SystemContractsCaller.systemCallWithPropagatedRevert(
+            uint32(gasleft()),
+            address(NONCE_HOLDER_SYSTEM_CONTRACT),
+            0,
+            abi.encodeCall(
+                INonceHolder.incrementMinNonceIfEquals,
+                (_transaction.nonce)
+            )
+        );
+
+        // Check for fee to pay
+        uint256 totalRequiredBalance = _transaction.totalRequiredBalance();
+        if (totalRequiredBalance > address(this).balance) {
+            revert ZkMinimalAccount__NotEnoughBalance();
+        }
+
+        // Check the signature
+        bytes32 txHash = _transaction.encodeHash();
+        // (_transaction.signature, txHash);
+        // bytes32 convertedHash = MessageHashUtils.toEthSignedMessageHash(txHash); PC said we don't need this..already in the correct format...
+        address signer = ECDSA.recover(convertedHash, _transaction.signature);
+        bool isValidSigner = signer == owner();
+        if (isValidSigner) {
+            magic = ACCOUNT_VALIDATION_SUCCESS_MAGIC;
+        } else {
+            magic = bytes4(0);
+        }
+
+        // return the "magic" number
+        return magic;
+    }
+
+    function _executeTransaction(Transaction memory _transaction) internal {
+        address to = address(uint160(_transaction.to));
+        uint128 value = Utils.safeCastToU128(_transaction.value);
+        bytes memory data = _transaction.data;
+
+        if (to == address(DEPLOYER_SYSTEM_CONTRACT)) {
+            uint32 gas = Utils.safeCastToU32(gasleft());
+            SystemContractsCaller.systemCallWithPropagatedRevert(
+                gas,
+                to,
+                value,
+                data
+            );
+        } else {
+            bool success;
+            assembly {
+                success := call(
+                    gas(),
+                    to,
+                    value,
+                    add(data, 0x20),
+                    mload(data),
+                    0,
+                    0
+                )
+            }
+            if (!success) {
+                revert ZkMinimalAccount__ExecutionFailed();
+            }
+        }
+    }
+}
